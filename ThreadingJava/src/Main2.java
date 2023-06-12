ea7cf1f7fb8cc48d51e5359901ea21a64abe4e63
iamaditya <aditya1s2h@gmail.com>
2023-06-12 07:29:32 +0530

    Sleep Thread

--- ThreadingJava/src/Main2.java
+++ ThreadingJava/src/Main2.java
@@ -3,7 +3,12 @@ public class Main2 extends Thread {
         for (int i=0;i<5;i++){
             if(i%2==0){
             System.out.println("i: "+i);
-        }
+            try{
+            Thread.sleep(1000);
+        }catch(Exception a){
+                System.out.println(a);
+                }
+            }
         }
     }
     public static void main(String[] args) {
